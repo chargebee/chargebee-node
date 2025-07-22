c08da126102e92fc60def9a47bcdc465bbd0cd82
cb-alish <alish.sapkota@chargebee.com>
2025-07-22 16:25:19 +0530

    Release version 3.11.1

--- src/util.ts
+++ src/util.ts
@@ -271,24 +271,10 @@ export function log(
   console.debug(logLine);
 }
 
-import { randomBytes } from 'crypto';
-
 export function generateUUIDv4(): string {
-  const bytes: Buffer = randomBytes(16);
-
-  // Set version to 4 (UUIDv4)
-  bytes[6] = (bytes[6] & 0x0f) | 0x40;
-
-  // Set variant to 10xxxxxx
-  bytes[8] = (bytes[8] & 0x3f) | 0x80;
-
-  const hex = bytes.toString('hex');
-
-  return [
-    hex.slice(0, 8),
-    hex.slice(8, 12),
-    hex.slice(12, 16),
-    hex.slice(16, 20),
-    hex.slice(20),
-  ].join('-');
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+      const r = (Math.random() * 16) | 0;
+      const v = c === 'x' ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
 }
